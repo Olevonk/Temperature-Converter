87d874f6c085b5195b61cdf58ee4178140ff5ef2
Olevonk <olevonk@gmail.com>
2024-05-02 23:39:06 -0500

    make converter

--- project02-01.js
+++ project02-01.js
@@ -3,9 +3,26 @@
       Project 02-01
 
       Celsius <-> Farenheit Coverter
-      Author: 
-      Date:   
+      Author: Oleksii Fon Krupp
+      Date:   06/03/2024
 
       Filename: project02-01.js
  */
 
+      function FahrenheitToCelsius(degree) {
+            return (degree - 32) / 1.8;
+        }
+        
+        function CelsiusToFahrenheit(degree) {
+            return degree * 1.8 + 32;
+        }
+
+        document.getElementById('cValue').onchange = function() {
+            var cDegree = document.getElementById('cValue').value;
+            document.getElementById('fValue').value = CelsiusToFahrenheit(cDegree);
+        };
+        
+        document.getElementById('fValue').onchange = function() {
+            var fDegree = document.getElementById('fValue').value;
+            document.getElementById('cValue').value = FahrenheitToCelsius(fDegree);
+        };
